1cdf99b3c8b2b34af3195d6b6904d1e43cc62290
Søren Roug <soren.roug@eea.europa.eu>
2002-10-07 14:45:36 +0000

    TE had forgotten to describe the rdfs:Class called Activity.

--- src/eionet/rod/RDFServlet.java
+++ src/eionet/rod/RDFServlet.java
@@ -98,6 +98,9 @@ public class RDFServlet extends BaseServletAC {
     s.append("<rdfs:range rdf:resource=\"#Activity\"/>");
     s.append("</rdf:Property>");
     
+    s.append("<rdfs:Class rdf:ID=\"Activity\"">);
+    s.append("<rdf:value>Activity</rdf:value>");
+    s.append("</rdfs:Class>");
     
     Vector acts = WebRODService.getActivities();
     
