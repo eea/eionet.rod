74ccb78f623e479824af5b211001ca692a0f8bbc
Tieto Estonia <info@tieto.ee>
2003-11-07 08:22:13 +0000

    new version

---
+++ src/eionet/rod/SpatialHistory.java
@@ -0,0 +1,54 @@
+/**
+ * The contents of this file are subject to the Mozilla Public
+ * License Version 1.1 (the "License"); you may not use this file
+ * except in compliance with the License. You may obtain a copy of
+ * the License at http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS
+ * IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing
+ * rights and limitations under the License.
+ *
+ * The Original Code is "EINRC-5 / WebROD Project".
+ *
+ * The Initial Developer of the Original Code is TietoEnator.
+ * The Original Code code was developed for the European
+ * Environment Agency (EEA) under the IDA/EINRC framework contract.
+ *
+ * Copyright (C) 2000-2002 by European Environment Agency.  All
+ * Rights Reserved.
+ *
+ * Original Code: Kaido Laine (TietoEnator)
+ */
+
+package eionet.rod;
+
+import java.io.*;
+import javax.servlet.http.*;
+
+import com.tee.util.*;
+import com.tee.xmlserver.*;
+
+public class SpatialHistory extends ROServletAC {
+   protected String setXSLT(HttpServletRequest req) {
+      return PREFIX + "spatialhistory.xsl";
+   }
+    
+   
+   protected DataSourceIF prepareDataSource(Parameters parameters){
+
+      String querySource = PREFIX + "spatialhistory.xrs";
+      String queryPars[][] = {{"ID", parameters.getParameter("ID")}};
+
+      DataSourceIF dataSrc = XMLSource.getXMLSource(querySource, parameters.getRequest());
+      dataSrc.setParameters(queryPars);
+
+      return dataSrc;
+   }
+   
+   protected SaveHandler setDataHandler() {
+      return null;
+   }
+
+
+}
