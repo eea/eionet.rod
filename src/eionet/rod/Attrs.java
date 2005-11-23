078a1a090bef7ac13cbabf518597ad49612a9104
Risto Alt <risto@tripledev.ee>
2005-11-23 13:10:19 +0000

    some attributes needed in UNS eventSender

---
+++ src/eionet/rod/Attrs.java
@@ -0,0 +1,14 @@
+package eionet.rod;
+
+/*
+ * 
+ * @author jaanus
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public interface Attrs {
+    public static final String USERPREFIX = "com.tee.xmlserver.user/";
+    public static final String APPPARAM = "application-name";
+    public static final String SCHEMA_RDF = "http://rod.eionet.eu.int/schema.rdf#";
+}
