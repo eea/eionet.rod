40cc4d8c8a2f52f84b6e8a442fa4125a7e700f1b
Risto Alt <risto@tripledev.ee>
2005-11-23 13:13:12 +0000

    UNSEventSender ... sends events about new obligations and obligation changes to UNS

---
+++ src/eionet/rod/UNSEventSender.java
@@ -0,0 +1,49 @@
+package eionet.rod;
+
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+
+import org.apache.xmlrpc.XmlRpcClient;
+
+import eionet.rod.services.FileServiceIF;
+import eionet.rod.services.RODServices;
+
+/**
+ * @author jaanus
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class UNSEventSender {
+	
+	public UNSEventSender(){
+	}
+
+	/*
+	 * 
+	 */
+	public static void makeCall(Object notifications) throws Exception{
+        try{
+            FileServiceIF fileSrv = RODServices.getFileService();
+            String server_url = fileSrv.getStringProperty(FileServiceIF.UNS_XMLRPC_SERVER_URL);
+            String channel_name = fileSrv.getStringProperty(FileServiceIF.UNS_CHANNEL_NAME);
+            if (notifications==null)
+                throw new Exception("Cannot send a null object via XML-RPC");
+            
+            XmlRpcClient server = new XmlRpcClient(server_url);
+            server.setBasicAuthentication("rod", "reportnet");
+            
+            Vector params = new Vector();
+            params.add(channel_name);
+            params.add(notifications);
+    
+            String result = null;
+            result = (String) server.execute("sendNotification", params);
+            
+        } catch (Throwable t) {
+            t.printStackTrace(System.out);
+            throw new ServletException(t);
+        }
+    }
+}
