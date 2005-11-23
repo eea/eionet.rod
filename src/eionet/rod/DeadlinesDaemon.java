181edaf00e81b8a6ea9422198d13c43f7f80afc9
Risto Alt <risto@tripledev.ee>
2005-11-23 13:11:09 +0000

    DeadlineDaemon for UNS approaching deadlines

---
+++ src/eionet/rod/DeadlinesDaemon.java
@@ -0,0 +1,194 @@
+package eionet.rod;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Hashtable;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+import javax.servlet.ServletException;
+
+import org.apache.xmlrpc.XmlRpcClient;
+
+import eionet.rod.services.FileServiceIF;
+import eionet.rod.services.RODServices;
+import eionet.rod.services.ServiceException;
+
+/**
+ * @author risto alt
+ *
+ * To change the template for this generated type comment go to
+ * Window>Preferences>Java>Code Generation>Code and Comments
+ */
+public class DeadlinesDaemon {
+    
+    public DeadlinesDaemon(){
+    }
+    
+	/*
+	 * 
+	 */
+	public static void main(String[] args) throws ServiceException {
+        
+        try{
+            
+            SimpleDateFormat formatter = new SimpleDateFormat("dd.MM.yy");
+            String d = formatter.format(new Date());
+            Date today = formatter.parse(d);
+            long time = today.getTime();
+            
+            String fileName = RODServices.getFileService().getStringProperty(FileServiceIF.DEADLINES_DAEMON_DATEFILE);
+            File file = new File(fileName);
+            file.createNewFile();
+            FileReader in = new FileReader(file);
+            BufferedReader br = new BufferedReader(in);
+            FileWriter out = null;
+            String result = br.readLine();
+            if(result != null){
+                Date date = formatter.parse(result);
+                if(date.before(today)){
+                    out = new FileWriter(file);
+                    out.write(d);
+                    makeStructure(date);
+                }
+            } else {
+                out = new FileWriter(file);
+                out.write(d);
+                makeStructure(today);
+            }
+            if (out != null) {
+                out.flush();
+                out.close();
+            }
+            in.close();
+            
+        } catch (Throwable t){
+            t.printStackTrace(System.out);
+        }
+	}
+    
+    private static void makeStructure(Date date) throws Exception{
+        
+        Vector lists = new Vector();
+        int percent = RODServices.getFileService().getIntProperty( FileServiceIF.PERCENT_OF_FREQ);
+        double days = (percent / 100.0) * 30.0;
+        
+            Vector vec = RODServices.getDbService().getUpcomingDeadlines(days);
+            long timestamp = System.currentTimeMillis();
+            
+            
+            for (Enumeration en = vec.elements(); en.hasMoreElements(); ){
+                Hashtable h = (Hashtable) en.nextElement();
+                
+                String nd = (String) h.get("next_deadline");
+                int year = Integer.parseInt(nd.substring(0,4)) - 1900;
+                int month = Integer.parseInt(nd.substring(5,7)) - 1;
+                int day = Integer.parseInt(nd.substring(8,10));
+                Date nextDeadline = new Date(year, month, day);
+                
+                long nextDeadlineMillis = nextDeadline.getTime();
+                
+                String freq = (String) h.get("freq");
+                int f = Integer.parseInt(freq);
+                int period = new Double(days * f).intValue();
+                long periodMillis = (new Long(period).longValue() * new Long(24).longValue() * new Long(3600).longValue() * new Long(1000).longValue());
+                Date periodStartDate = new Date(nextDeadlineMillis - periodMillis);
+                
+                if(!periodStartDate.before(date)){
+                
+                    Vector list = new Vector();
+                    String events = "http://rod.eionet.eu.int/events/" + timestamp;
+                    
+                    list.add(events);
+                    list.add("http://www.w3.org/1999/02/22-rdf-syntax-ns#type");
+                    list.add(Attrs.SCHEMA_RDF + "Deadlineevent");
+                    lists.add(list);
+                    
+                    list = new Vector();
+                    list.add(events);
+                    list.add(Attrs.SCHEMA_RDF + "event_type");
+                    list.add("Deadlineevent");
+                    lists.add(list);
+                    
+                    list = new Vector();
+                    list.add(events);
+                    list.add(Attrs.SCHEMA_RDF + "label");
+                    list.add("Approaching deadline");
+                    lists.add(list);
+                    
+                    list = new Vector();
+                    list.add(events);
+                    list.add(Attrs.SCHEMA_RDF + "nextdeadline");
+                    list.add(h.get("next_deadline"));
+                    lists.add(list);
+                    
+                    list = new Vector();
+                    list.add(events);
+                    list.add(Attrs.SCHEMA_RDF + "nextdeadline2");
+                    list.add(h.get("next_deadline2"));
+                    lists.add(list);
+                    
+                    list = new Vector();
+                    list.add(events);
+                    list.add(Attrs.SCHEMA_RDF + "obligation");
+                    list.add(h.get("title"));
+                    lists.add(list);
+                    
+                    String ids = (String) h.get("country_ids");
+                    StringTokenizer st = new StringTokenizer(ids, ",");
+                    while (st.hasMoreTokens()) {
+                        String token = st.nextToken().trim();
+                        if (token != null && !token.equalsIgnoreCase(" ") && !token.equalsIgnoreCase("")){
+                            String country = RODServices.getDbService().getCountryById(token);
+                            if (country != null) {
+                                list = new Vector();
+                                list.add(events);
+                                list.add(Attrs.SCHEMA_RDF + "locality");
+                                list.add(country);
+                                lists.add(list);
+                            }
+                        }
+                    }
+                    
+                    list = new Vector();
+                    list.add(events);
+                    list.add(Attrs.SCHEMA_RDF + "responsiblerole");
+                    list.add(h.get("responsible_role"));
+                    lists.add(list);
+                    
+                    if (lists.size() > 0){
+                        makeCall(lists);
+                    }
+                }
+            }
+    }
+    
+    public static void makeCall(Object notifications) throws Exception{
+        try{
+            FileServiceIF fileSrv = RODServices.getFileService();
+            String server_url = fileSrv.getStringProperty(FileServiceIF.UNS_XMLRPC_SERVER_URL);
+            String channel_name = fileSrv.getStringProperty(FileServiceIF.UNS_CHANNEL_NAME);
+            if (notifications==null)
+                throw new Exception("Cannot send a null object via XML-RPC");
+            
+            XmlRpcClient server = new XmlRpcClient(server_url);
+            server.setBasicAuthentication("rod", "reportnet");
+            
+            Vector params = new Vector();
+            params.add(channel_name);
+            params.add(notifications);
+    
+            String result = null;
+            result = (String) server.execute("sendNotification", params);
+    
+        } catch (Throwable t) {
+            t.printStackTrace(System.out);
+            throw new ServletException(t);
+        }
+    }
+}
