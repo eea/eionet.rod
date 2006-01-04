6cbad1bd0b8d9d9ef64b5dd657060a5bb5a72d52
Søren Roug <soren.roug@eea.europa.eu>
2006-01-04 14:06:23 +0000

    Made it work

--- deadlines-daemon.sh
+++ deadlines-daemon.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+#
+# Even though this file is called a daemon, it has to be run
+# from crontab once a day
+#
 LOCKFILE=/var/lock/rod-deadlines-daemon.lock
 [ -f $LOCKFILE ] && exit 0
 trap "rm -f $LOCKFILE" EXIT
@@ -8,7 +12,10 @@ touch $LOCKFILE
 java=/usr/lib/jvm/java/bin/java
 
 # place the right path in here !!!
-ad=/var/lib/tomcat4/webapps/webrod/public/WEB-INF
+rod=/var/lib/tomcat4/webapps/webrod/public/WEB-INF
+cd $rod/classes
+
+libpath=$rod/lib
 
 # place the right path in here !!!
 cp=/usr/share/java/mysql-connector-java.jar
@@ -16,7 +23,7 @@ cp=/usr/share/java/mysql-connector-java.jar
 # place the right path in here !!!
 cp=$cp:/usr/share/java/servlet.jar
 
-cp=$cp:$ad/classes
-cp=$cp:$ad/lib/tomcat-util.jar:$ad/lib/xmlrpc.jar:$ad/lib/xmlserver.jar:$CLASSPATH
+cp=$cp:$libpath/rod.jar
+cp=$cp:$libpath/tomcat-util.jar:$libpath/xmlrpc.jar:$libpath/xmlserver.jar:$CLASSPATH
 
 $java -cp $cp eionet.rod.DeadlinesDaemon
