7d88f5c39806a0d5befa199eed2ac6c5515e437c
Søren Roug <soren.roug@eea.europa.eu>
2007-05-24 13:06:08 +0000

    Done the change

--- ACL_ROWS.STATUS into primary key.sql
+++
@@ -1,5 +0,0 @@
--- puts ACL_ROWS.STATUS column back into primary key
-----------------------------------------------------
-
-alter table ACL_ROWS drop primary key;
-alter table ACL_ROWS add primary key (ACL_ID, TYPE, ENTRY_TYPE, PRINCIPAL, STATUS);
