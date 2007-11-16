4e7217160c183e0a0f111598dbe464f1a6bead12
Risto Alt <risto@tripledev.ee>
2007-11-16 10:50:59 +0000

    Ticket #1033

--- src/eionet/rod/services/modules/db/dao/mysql/RoleMySqlDao.java
+++ src/eionet/rod/services/modules/db/dao/mysql/RoleMySqlDao.java
@@ -5,7 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Hashtable;
+import java.util.Vector;
 
+import eionet.directory.DirServiceException;
+import eionet.directory.DirectoryService;
 import eionet.rod.services.ServiceException;
 import eionet.rod.services.modules.db.dao.IRoleDao;
 
@@ -83,7 +86,11 @@ public class RoleMySqlDao extends MySqlBaseDao implements IRoleDao {
 
 	private static final String q_insert_role = 
 		"INSERT INTO T_ROLE " + 
-		"SET STATUS=?, LAST_HARVESTED={fn now()}, ROLE_NAME=?, ROLE_EMAIL=?, ROLE_ID=?, ROLE_URL=?, ROLE_MEMBERS_URL=?, PERSON=?, INSTITUTE=?";
+		"SET STATUS=?, LAST_HARVESTED={fn now()}, ROLE_NAME=?, ROLE_EMAIL=?, ROLE_ID=?, ROLE_URL=?, ROLE_MEMBERS_URL=?";
+	
+	private static final String q_insert_role_occupants = 
+		"INSERT INTO T_ROLE_OCCUPANTS " + 
+		"SET ROLE_ID=?, PERSON=?, INSTITUTE=?";
 
 	private static final String q_update_role_rollback = 
 		"UPDATE T_ROLE " + 
@@ -93,10 +100,115 @@ public class RoleMySqlDao extends MySqlBaseDao implements IRoleDao {
 	/*
 	 * (non-Javadoc)
 	 * 
-	 * @see eionet.rod.services.modules.db.dao.IRoleDao#saveRole(java.util.Hashtable,
-	 *      java.lang.String, java.lang.String)
+	 * @see eionet.rod.services.modules.db.dao.IRoleDao#saveRole(java.util.Hashtable)
 	 */
-	public void saveRole(Hashtable role, String person, String org) throws ServiceException {
+	public void saveRole(Hashtable role) throws ServiceException {
+		// backup ->
+		String roleId = (String) role.get("ID");
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+
+		try {
+			connection = getConnection();
+			preparedStatement = connection.prepareStatement(q_delete_role_data);
+			preparedStatement.setString(1, roleId);
+			if (isDebugMode) logQuery(q_delete_role_data);
+			preparedStatement.executeUpdate();
+			preparedStatement.close();
+			
+			String uid="";
+			String orgId="";
+			String orgName=orgId;
+			String fullName=uid;
+			Hashtable person = null;
+			Hashtable org = null;
+
+			Vector occupants = (Vector)role.get("OCCUPANTS");
+			if (occupants!=null && occupants.size()>0){
+				for(int i = 0; i<occupants.size(); i++){
+					uid = (String)occupants.elementAt(i);
+				
+					if (uid!=null && uid.trim().length()>0){
+						try {
+							person=DirectoryService.getPerson(uid);
+						}
+						catch (DirServiceException dire) {
+							logger.error("Error getting person " + uid + ": " + dire.toString());
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+		
+					if (person != null) {
+						fullName=(String)person.get("FULLNAME");
+						orgId=(String)person.get("ORG_ID");
+					}
+		
+					if (orgId!=null && orgId.trim().length()>0){
+						try {
+							org =  DirectoryService.getOrganisation(orgId);
+							if (org!=null)
+								orgName=(String)org.get("NAME");
+						}
+						catch (DirServiceException dire) {
+							logger.error("Error getting organisation " + orgId + ": " + dire.toString());
+						}
+						catch (Exception e) {
+							e.printStackTrace();
+						}
+					}
+					preparedStatement = connection.prepareStatement(q_insert_role_occupants);
+					preparedStatement.setString(1, roleId);
+					preparedStatement.setString(2, fullName);
+					preparedStatement.setString(3, orgName);
+					if (isDebugMode) logQuery(q_insert_role_occupants);
+					preparedStatement.executeUpdate();
+					preparedStatement.close();
+				}
+			}
+
+			String circaUrl = (String) role.get("URL");
+			String circaMembersUrl = (String) role.get("URL_MEMBERS");
+			String desc = (String) role.get("DESCRIPTION");
+			String mail = (String) role.get("MAIL");
+
+			if (roleId != null) {
+				preparedStatement = connection.prepareStatement(q_insert_role);
+				preparedStatement.setInt(1, 1);
+				preparedStatement.setString(2, desc);
+				preparedStatement.setString(3, mail);
+				preparedStatement.setString(4, roleId);
+				preparedStatement.setString(5, circaUrl);
+				preparedStatement.setString(6, circaMembersUrl);
+				if (isDebugMode) logQuery(q_insert_role);
+				preparedStatement.executeUpdate();
+				preparedStatement.close();
+			}
+		} catch (Exception e) {
+			logger.error(e);
+			try {
+				preparedStatement = connection.prepareStatement(q_delete_role_data);
+				preparedStatement.setString(1, roleId);
+				if (isDebugMode) logQuery(q_delete_role_data);
+				preparedStatement.executeUpdate();
+				preparedStatement.close();
+				preparedStatement = connection.prepareStatement(q_update_role_rollback);
+				preparedStatement.setString(1, roleId);
+				if (isDebugMode) logQuery(q_update_role_rollback);
+				preparedStatement.executeUpdate();
+				preparedStatement.close();
+			} catch (SQLException e1) {
+				logger.error(e1);
+				throw new ServiceException(e1.getMessage());
+			}
+		} finally {
+			closeAllResources(null, preparedStatement, connection);
+		}
+
+	}
+	
+	/*public void saveRole(Hashtable role, String person, String org) throws ServiceException {
 		// backup ->
 		String roleId = (String) role.get("ID");
 		Connection connection = null;
@@ -150,7 +262,7 @@ public class RoleMySqlDao extends MySqlBaseDao implements IRoleDao {
 			closeAllResources(null, preparedStatement, connection);
 		}
 
-	}
+	}*/
 
 	private static final String q_update_role_status = "UPDATE T_ROLE " + "SET STATUS=?";
 
@@ -203,10 +315,21 @@ public class RoleMySqlDao extends MySqlBaseDao implements IRoleDao {
 
 	private static final String q_role_description = 
 		"SELECT " + 
-			"PERSON AS person, " + 
-			"INSTITUTE AS institute, " + 
-			"ROLE_URL AS role_url " + 
+			"ROLE_NAME AS name, " +
+			"ROLE_ID AS role_id, " +
+			"ROLE_EMAIL AS email, " + 
+			"ROLE_URL AS role_url, " + 
+			"ROLE_ID AS role_id, " +
+			"ROLE_MEMBERS_URL AS members_url, " +
+			"LAST_HARVESTED AS last_harvested " +
 		"FROM T_ROLE " + 
+		"WHERE ROLE_ID =? ";
+	
+	private static final String q_role_occupants = 
+		"SELECT " + 
+			"PERSON AS person, " + 
+			"INSTITUTE AS institute " + 
+		"FROM T_ROLE_OCCUPANTS " + 
 		"WHERE ROLE_ID =? ";
 
 	/*
@@ -218,14 +341,24 @@ public class RoleMySqlDao extends MySqlBaseDao implements IRoleDao {
 
 		Connection connection = null;
 		PreparedStatement preparedStatement = null;
-		Hashtable result = null;
+		Hashtable result = new Hashtable();
+		Vector occupants = new Vector();
 
 		try {
 			connection = getConnection();
+			preparedStatement = connection.prepareStatement(q_role_occupants);
+			preparedStatement.setString(1, role_id);
+			logQuery(q_role_occupants);
+			occupants = _getVectorOfHashes(preparedStatement);
+			preparedStatement.close();
+						
 			preparedStatement = connection.prepareStatement(q_role_description);
 			preparedStatement.setString(1, role_id);
 			logQuery(q_role_description);
 			result = _getHashtable(preparedStatement);
+			
+			if(occupants != null && result != null)
+				result.put("occupants", occupants);
 
 		} catch (SQLException exception) {
 			logger.error(exception);
@@ -235,6 +368,42 @@ public class RoleMySqlDao extends MySqlBaseDao implements IRoleDao {
 		}
 
 		return result != null ? result : new Hashtable();
+	}
+	
+	private static final String q_role_obligations = 
+		"SELECT " + 
+			"PK_RA_ID AS ra_id, " + 
+			"FK_SOURCE_ID AS sid, " +
+			"TITLE AS title " +
+		"FROM T_OBLIGATION " + 
+		"WHERE RESPONSIBLE_ROLE =? ";
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see eionet.rod.services.modules.db.dao.IRoleDao#getRoleObligations(java.lang.String)
+	 */
+	public Vector getRoleObligations(String role_id) throws ServiceException {
+
+		Connection connection = null;
+		PreparedStatement preparedStatement = null;
+		Vector result = null;
+
+		try {
+			connection = getConnection();
+			preparedStatement = connection.prepareStatement(q_role_obligations);
+			preparedStatement.setString(1, role_id);
+			logQuery(q_role_obligations);
+			result = _getVectorOfHashes(preparedStatement);
+			
+		} catch (SQLException exception) {
+			logger.error(exception);
+			throw new ServiceException(exception.getMessage());
+		} finally {
+			closeAllResources(null, preparedStatement, connection);
+		}
+
+		return result != null ? result : new Vector();
 	}
 
 }
