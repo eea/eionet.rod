ea5f80e2271ebd541f1d76c18cefa16fb43acd2b
Risto Alt <risto@tripledev.ee>
2008-12-03 11:12:30 +0000

    Ticket #1664

---
+++ db/rodupdate031208.sql
@@ -0,0 +1,3 @@
+INSERT INTO HLP_SCREEN VALUES ('Disclaimer_page', 'The page containing disclaimer text');
+INSERT INTO HLP_SCREEN VALUES ('Help_page', 'The page containing general help text');
+INSERT INTO HLP_SCREEN VALUES ('RSSHelp_page', 'The page containing RSS XML/RPC data extraction help text');
