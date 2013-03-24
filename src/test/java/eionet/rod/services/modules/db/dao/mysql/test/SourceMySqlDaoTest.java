f37755cea7c0927c52f24b25c586fd60cc451a7f
Søren Roug <soren.roug@eea.europa.eu>
2013-03-24 13:57:29 +0000

    Don't print out from unit tests

--- src/test/java/eionet/rod/services/modules/db/dao/mysql/test/SourceMySqlDaoTest.java
+++ src/test/java/eionet/rod/services/modules/db/dao/mysql/test/SourceMySqlDaoTest.java
@@ -1,41 +1,46 @@
 package eionet.rod.services.modules.db.dao.mysql.test;
 
+import java.util.Map;
+import java.util.Vector;
+
 import eionet.rod.services.modules.db.dao.mysql.SourceMySqlDao;
 
 public class SourceMySqlDaoTest extends BaseMySqlDaoTest {
 
-	private SourceMySqlDao sourceMySqlDao = new SourceMySqlDao();
-	public static void main(String[] args) {
-		junit.textui.TestRunner.run(SourceMySqlDaoTest.class);
-	}
+    private SourceMySqlDao sourceMySqlDao = new SourceMySqlDao();
 
-	public SourceMySqlDaoTest(String arg0) {
-		super(arg0);
-	}
+    public static void main(String[] args) {
+        junit.textui.TestRunner.run(SourceMySqlDaoTest.class);
+    }
 
-	protected void setUp() throws Exception {
-		super.setUp();
-	}
+    public SourceMySqlDaoTest(String arg0) {
+        super(arg0);
+    }
 
-	protected void tearDown() throws Exception {
-		super.tearDown();
-	}
+    protected void setUp() throws Exception {
+        super.setUp();
+    }
 
-	/*
-	 * Test method for 'eionet.rod.services.modules.db.dao.mysql.SourceMySqlDao.getInstruments()'
-	 */
-	public void testGetInstruments() throws Exception{
-		printVectorResult(sourceMySqlDao.getInstruments());
-	}
+    protected void tearDown() throws Exception {
+        super.tearDown();
+    }
 
-	/*
-	 * Test method for 'eionet.rod.services.modules.db.dao.mysql.SourceMySqlDao.getInstrumentsRSS()'
-	 */
-	public void testGetInstrumentsRSS() throws Exception{
-		String [][] m = sourceMySqlDao.getInstrumentsRSS();
-		assertEquals(2, m.length);
-//		printMatrixResult(sourceMySqlDao.getInstrumentsRSS());
+    /*
+     * Test method for 'eionet.rod.services.modules.db.dao.mysql.SourceMySqlDao.getInstruments()'
+     */
+    public void testGetInstruments() throws Exception{
+        Vector<Map<String, String>> vector = sourceMySqlDao.getInstruments();
+        assertEquals(2, vector.size());
+    }
 
-	}
+    /*
+     * Test method for 'eionet.rod.services.modules.db.dao.mysql.SourceMySqlDao.getInstrumentsRSS()'
+     */
+    public void testGetInstrumentsRSS() throws Exception {
+        String [][] m = sourceMySqlDao.getInstrumentsRSS();
+        assertEquals(2, m.length);
+//      printMatrixResult(sourceMySqlDao.getInstrumentsRSS());
+
+    }
 
 }
