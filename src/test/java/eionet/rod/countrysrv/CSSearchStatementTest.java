7dff9cbde20e810b3ab3d03a14d55d62b1c360af
Søren Roug <soren.roug@eea.europa.eu>
2013-03-26 12:07:35 +0000

    The base class had moved

--- src/test/java/eionet/rod/countrysrv/CSSearchStatementTest.java
+++ src/test/java/eionet/rod/countrysrv/CSSearchStatementTest.java
@@ -1,26 +1,26 @@
 package eionet.rod.countrysrv;
 
-import eionet.rod.services.modules.db.dao.mysql.test.BaseMySqlDaoTest;
+import eionet.rod.services.modules.db.dao.mysql.BaseMySqlDaoTest;
 
 public class CSSearchStatementTest extends BaseMySqlDaoTest {
-	
-	public CSSearchStatementTest(String arg0) {
-		super(arg0);
-	}
-	
-	public void testDummyTest() { }
+
+    public CSSearchStatementTest(String arg0) {
+        super(arg0);
+    }
+
+    public void testDummyTest() { }
 
     //  check for Unknown column 'T_SPATIAL.SPATIAL_ISMEMBERCOUNTRY' in 'on clause'
     /*public void test_query() throws Exception
     {
-                // Create the mock objects
-                HttpServletRequest request = createMock(HttpServletRequest.class);
-                String reqString = "COUNTRY_ID=3&ORD=NEXT_REPORTING,%20DEADLINE";
-                Parameters params = new Parameters(request, reqString);
-                CSSearchStatement stmt = new CSSearchStatement(params, false, false);
-                Connection conn = getConnection().getConnection();
-                PreparedStatement ps = stmt.prepareStatement(conn);
-                stmt.executeQuery();
+        // Create the mock objects
+        HttpServletRequest request = createMock(HttpServletRequest.class);
+        String reqString = "COUNTRY_ID=3&ORD=NEXT_REPORTING,%20DEADLINE";
+        Parameters params = new Parameters(request, reqString);
+        CSSearchStatement stmt = new CSSearchStatement(params, false, false);
+        Connection conn = getConnection().getConnection();
+        PreparedStatement ps = stmt.prepareStatement(conn);
+        stmt.executeQuery();
     }*/
 
 }
