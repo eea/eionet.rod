76b435c1795994e4e38dc6480ba2afc451ea5d5a
Søren Roug <soren.roug@eea.europa.eu>
2014-08-02 17:12:35 +0200

    An explanation for why we fake the date.

--- src/test/java/eionet/rod/rdf/FakeEvents.java
+++ src/test/java/eionet/rod/rdf/FakeEvents.java
@@ -5,6 +5,10 @@ import java.util.Date;
 
 public class FakeEvents extends Events {
 
+    /*
+     * Since the dates in seed-events.xml are fixed to around 2007 and 2008,
+     * we set today's date to something that will guarantee an output.
+     */
     @Override
     protected Date getTodaysDate() {
         return dateFromString("2006-12-25");
