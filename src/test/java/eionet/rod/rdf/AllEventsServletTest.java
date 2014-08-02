99b9c134c6832c6383da282446ae3e2f7e3e1cf0
Søren Roug <soren.roug@eea.europa.eu>
2014-08-02 09:52:46 +0200

    Add tests with parameters and non-ASCII data.

--- src/test/java/eionet/rod/rdf/AllEventsServletTest.java
+++ src/test/java/eionet/rod/rdf/AllEventsServletTest.java
@@ -38,22 +38,65 @@ public class AllEventsServletTest extends BaseMySqlDaoTest {
      * until 36 days before.
      */
     @Test
-    public void testSimpleTest() throws  Exception {
+    public void testNoParameters() throws  Exception {
         ServletRunner sr = new ServletRunner();
         sr.registerServlet("allevents.rss", AllEvents.class.getName());
-
         ServletUnitClient sc = sr.newClient();
-        WebRequest request   = new GetMethodWebRequest( "http://test.meterware.com/allevents.rss" );
-        //request.setParameter( "color", "red" );
-        WebResponse response = sc.getResponse( request );
-        Assert.assertNotNull( "No response received", response );
-        Assert.assertEquals( "content type", "application/rss+xml", response.getContentType() );
+        WebRequest request   = new GetMethodWebRequest("http://test.meterware.com/allevents.rss");
+        WebResponse response = sc.getResponse(request);
+        Assert.assertNotNull("No response received", response);
+        Assert.assertEquals("content type", "application/rss+xml", response.getContentType());
         String resp = response.getText();
         //System.out.println(resp);
+
+        // Obl. 15
+        Assert.assertTrue(resp.contains("<ev:startdate>2007-01-01</ev:startdate>"));
         Assert.assertTrue(resp.contains("<title>Deadline for Reporting Obligation: &lt;&#038;&gt;</title>"));
-        Assert.assertTrue(resp.contains("<ev:startdate>2008-08-31</ev:startdate>"));
+        Assert.assertTrue(resp.contains("<description>&quot;&#038;amp;&quot;</description>"));
+        // Obl. 15 a year later
         Assert.assertTrue(resp.contains("<ev:startdate>2008-01-01</ev:startdate>"));
+        // Obl. 514
+        Assert.assertTrue(resp.contains("<title>Deadline for Reporting Obligation: Obl. 514—&gt;&#038;&lt;</title>"));
+        Assert.assertTrue(resp.contains("<ev:startdate>2008-08-31</ev:startdate>"));
+        Assert.assertTrue(resp.contains("<description>&#038;&#038;&quot;—</description>"));
+    }
+
+    @Test
+    public void testWithIssuesParameter() throws  Exception {
+        ServletRunner sr = new ServletRunner();
+        sr.registerServlet("allevents.rss", AllEvents.class.getName());
+        ServletUnitClient sc = sr.newClient();
+        WebRequest request   = new GetMethodWebRequest("http://test.meterware.com/allevents.rss");
+        // issue 1 OR 4. No obligation in test data has #4
+        request.setParameter("issues", "1,4");
+        WebResponse response = sc.getResponse(request);
+        Assert.assertNotNull("No response received", response);
+        Assert.assertEquals("content type", "application/rss+xml", response.getContentType());
+        String resp = response.getText();
+        //System.out.println(resp);
+
+        // Obl. 15
+        Assert.assertTrue(resp.contains("<ev:startdate>2008-01-01</ev:startdate>"));
+        Assert.assertTrue(resp.contains("<title>Deadline for Reporting Obligation: &lt;&#038;&gt;</title>"));
         Assert.assertTrue(resp.contains("<description>&quot;&#038;amp;&quot;</description>"));
     }
 
-}
+    @Test
+    public void testWithCountriesParameter() throws  Exception {
+        ServletRunner sr = new ServletRunner();
+        sr.registerServlet("allevents.rss", AllEvents.class.getName());
+        ServletUnitClient sc = sr.newClient();
+        WebRequest request   = new GetMethodWebRequest("http://test.meterware.com/allevents.rss");
+        request.setParameter("countries", "1,2,3");
+        WebResponse response = sc.getResponse(request);
+        Assert.assertNotNull("No response received", response);
+        Assert.assertEquals("content type", "application/rss+xml", response.getContentType());
+        String resp = response.getText();
+        //System.out.println(resp);
+
+        // Obl. 15
+        Assert.assertTrue(resp.contains("<ev:startdate>2007-01-01</ev:startdate>"));
+        Assert.assertTrue(resp.contains("<title>Deadline for Reporting Obligation: &lt;&#038;&gt;</title>"));
+        Assert.assertTrue(resp.contains("<description>&quot;&#038;amp;&quot;</description>"));
+    }
+} 
