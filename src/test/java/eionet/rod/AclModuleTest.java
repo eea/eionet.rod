e890a75596f9d787ba9c58bdc4a348c1850cc3d6
Søren Roug <soren.roug@eea.europa.eu>
2015-04-02 15:29:06 +0200

    Integration test for ACL module.

---
+++ src/test/java/eionet/rod/AclModuleTest.java
@@ -0,0 +1,22 @@
+package eionet.rod;
+
+import eionet.acl.AccessController;
+import org.junit.Test;
+import static org.junit.Assert.assertTrue;
+
+
+/**
+ * Integration test to verify that the eionet.acl module works correctly.
+ * As long as ROD doesn't configure where to load the acl module's configuration
+ * this test shows that the fallback to properties file works.
+ */
+public class AclModuleTest {
+
+    @Test
+    public void topLevelAclMustExist() throws Exception {
+        boolean top = AccessController.aclExists("/");
+        assertTrue("There must be a toplevel ACL", top);
+    }
+
+}
+
